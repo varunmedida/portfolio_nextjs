df824c6bd3a00227ca5850376a7952945a7f9c63
Varun Medida <vmedida1@student.gsu.edu>
2024-08-20 05:48:09 -0400

    Added footer and skills.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
+import Header from '@/components/header';
 import './globals.css';
 import { Inter } from 'next/font/google';
-import Header from '@/components/header';
+import ActiveSectionContextProvider from '@/context/active-section-context';
+import Footer from '@/components/footer';
+import ThemeSwitch from '@/components/theme-switch';
+import ThemeContextProvider from '@/context/theme-context';
+import { Toaster } from 'react-hot-toast';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,14 +20,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' className='!scroll-smooth'>
       <body
         className={`${inter.className} bg-slate-100 text-slate-950 relative h-[5000px] pt-28 sm:pt-36`}
       >
         <div className='bg-[#7c87e8] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]'></div>
         <div className='bg-[#8acce4] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]'></div>
-        <Header></Header>
-        {children}
+        <ThemeContextProvider>
+          <ActiveSectionContextProvider>
+            <Header></Header>
+            {children}
+            <Footer />
+
+            <Toaster position='top-right' />
+            <ThemeSwitch />
+          </ActiveSectionContextProvider>
+        </ThemeContextProvider>
       </body>
     </html>
   );
