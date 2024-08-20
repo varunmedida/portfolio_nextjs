6317ce5745e8110b3ad5f8bf78fe17ffc64fe4aa
Varun Medida <vmedida1@student.gsu.edu>
2024-08-20 17:57:18 -0400

    Completed the portfolio

--- components/contact.tsx
+++ components/contact.tsx
@@ -42,7 +42,6 @@ export default function Contact() {
       <form
         className='mt-10 flex flex-col dark:text-black'
         action={async (formData) => {
-          // await sendEmail(formData);
           const { data, error } = await sendEmail(formData);
 
           if (error) {
