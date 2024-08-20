f6ff1019e5d2e01c0918b52a2a3d1d435cc09426
Varun Medida <vmedida1@student.gsu.edu>
2024-08-20 05:50:40 -0400

    Added children for Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
             <Footer />
 
             <Toaster position='top-right' />
-            <ThemeSwitch />
+            <ThemeSwitch>dark</ThemeSwitch>
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
       </body>
